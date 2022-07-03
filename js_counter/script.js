b4e966b02e251702a4e3d65c141f4633a867dfef
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-03 23:03:32 +0100

    js counter

---
+++ js_counter/script.js
@@ -0,0 +1,10 @@
+//..set initial count 
+let count = 0
+
+//..declare the value and buttons 
+const value = document.querySelector("#value")
+const allBtns = document.querySelectorAll(".btns")
+
+allBtns.forEach(function (items) {
+    console.log(items)
+})
