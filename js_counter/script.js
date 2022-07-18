b1c2c20b2522ebc9f3bf196ef3f32ff9a96d0d52
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-18 22:31:50 +0100

    js_counter

--- js_counter/script.js
+++ js_counter/script.js
@@ -7,8 +7,7 @@ const allBtns = document.querySelectorAll(".btns")
 
 allBtns.forEach(function (btn) {
     //..event lisnter to loop through the 3 buttons
-    btn.addEventListener("click", function(e){
+    btn.addEventListener('click', function(e){
         console.log(e.currentTarget)
     }) 
-    
 })
