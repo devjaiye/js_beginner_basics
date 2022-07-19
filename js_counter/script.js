f62185cdd8d47c931925c5a020374b85347d5023
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-19 22:14:53 +0100

    js_counter

--- js_counter/script.js
+++ js_counter/script.js
@@ -6,7 +6,24 @@ const value = document.querySelector("#value")
 const allBtns = document.querySelectorAll(".btn")
 
 allBtns.forEach(function (item){
-   item.addEventListener("click", function(e){
-    console.log(e.currentTarget.classList)
-   })
+   item.addEventListener("click", function(e){ //.. e is the event object 
+   const style = e.currentTarget.classList //..get the current class list of the counter
+   if(style.contains('decrease')){
+    count--
+   } else if(style.contains('increase')){
+         count++
+   }
+   else {
+      count = 0
+   } 
+   if (count > 0){
+      value.style.color = 'green'
+   } else if(count < 0){
+      value.style.color = 'red'
+   } else{
+      value.style.color = '#222'
+   }
+
+   value.textContent = count
+})
 })
